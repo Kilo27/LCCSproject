1139159273d6da12d1d3be2de2bfd2c453900d91
Kilo27 <kyle.oseoighe@gmail.com>
2024-12-16 10:05:48 +0000

    requests

--- .venv/main.py
+++ .venv/main.py
@@ -1,4 +1,13 @@
-import csv
-import requests
+from urllib.request import urlopen
+import certifi
+import json
 
-requests.get("")
+def get_jsonparsed_data(url):
+    response = urlopen(url, cafile=certifi.where())
+    data = response.read().decode("utf-8")
+    return json.loads(data)
+
+tickercode="KRZ"
+
+url = (f"https://financialmodelingprep.com/api/v3/quote/{tickercode}?apikey=HB0CyUa7I01Oaj8FsR3sy4kk2NovfzKf")
+print(get_jsonparsed_data(url))
