55720374bf54e6be241d7047d01d53bb7dfb7bdf
Kilo27 <kyle.oseoighe@gmail.com>
2024-12-18 22:05:59 +0000

    flask remains broken

--- artefact/app.py
+++ artefact/app.py
@@ -3,6 +3,6 @@ app=Flask("__name__")
 
 @app.route("/")
 def mainpage():
-    return render_template("template/app.html/")
+    return render_template('main.html')
 if __name__ == "__main__":
     app.run()
