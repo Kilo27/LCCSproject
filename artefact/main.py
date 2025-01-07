d0bf5497f764d15362ddeb35978edfcfc071b468
Kilo27 <kyle.oseoighe@gmail.com>
2025-01-07 16:15:47 +0000

    data handling

--- artefact/main.py
+++ artefact/main.py
@@ -1,7 +1,9 @@
 from urllib.request import urlopen
 import json
+import time
 
 akey="HB0CyUa7I01Oaj8FsR3sy4kk2NovfzKf"
+otherakey="a3d65b6e16f2672529ff996d85d98188"
 def get_jsonparsed_data(url):
     response = urlopen(url)
     data = response.read().decode("utf-8")
@@ -11,10 +13,14 @@ def get_jsonparsed_data(url):
 #corpinfo=get_jsonparsed_data()
 tickercode="GOOG"
 
-quote = f"https://financialmodelingprep.com/api/v3/quote/{tickercode}?apikey={akey}"
-simplequote=f"https://financialmodelingprep.com/api/v3/quote-short/{tickercode}?apikey={akey}"
-dailychange=f"https://financialmodelingprep.com/api/v3/stock-price-change/{tickercode}?apikey={akey}"
 
-print(get_jsonparsed_data(quote))
-print(get_jsonparsed_data(simplequote))
-print(get_jsonparsed_data(dailychange))
+#quote = f"https://financialmodelingprep.com/api/v3/quote/{tickercode}?apikey={akey}"
+#simplequote=f"https://financialmodelingprep.com/api/v3/quote-short/{tickercode}?apikey={akey}"
+#dailychange=f"https://financialmodelingprep.com/api/v3/stock-price-change/{tickercode}?apikey={akey}"
+#pastcloses=f"https://financialmodelingprep.com/api/v4/batch-historical-eod?date=2021-05-18/{tickercode}?apikey={akey}"
+
+pastinfo=f"https://api.marketstack.com/v1/eod?access_key={otherakey}&symbols={tickercode}"
+print(f"https://api.marketstack.com/v1/eod?access_key={otherakey}&symbols={tickercode}")
+
+print(get_jsonparsed_data(pastinfo))
+print(time.asctime())
