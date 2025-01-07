13ad0033bd05cfae79d0e409692e2eb6d8f608e8
Kilo27 <kyle.oseoighe@gmail.com>
2025-01-07 16:36:39 +0000

    changing the way stuff is obtained probably getting rid of the api cuz it brokey

--- artefact/main.py
+++ artefact/main.py
@@ -5,20 +5,13 @@ import time
 akey="HB0CyUa7I01Oaj8FsR3sy4kk2NovfzKf"
 otherakey="a3d65b6e16f2672529ff996d85d98188"
 def get_jsonparsed_data(url):
-    response = urlopen(url)
-    print(response)
-    data = response.read().decode("utf-8")
-    return json.loads(data)
-#url=""
-#name=input()
-#corpinfo=get_jsonparsed_data()
+    try:
+        with urlopen(url) as url:
+            data = json.load(url)
+            print(data)
+    except:
+        return "Unable to Process"
 tickercode="GOOG"
-
-
-#quote = f"https://financialmodelingprep.com/api/v3/quote/{tickercode}?apikey={akey}"
-#simplequote=f"https://financialmodelingprep.com/api/v3/quote-short/{tickercode}?apikey={akey}"
-#dailychange=f"https://financialmodelingprep.com/api/v3/stock-price-change/{tickercode}?apikey={akey}"
-#pastcloses=f"https://financialmodelingprep.com/api/v4/batch-historical-eod?date=2021-05-18/{tickercode}?apikey={akey}"
 
 pastinfo=f"https://api.marketstack.com/v1/eod?access_key={otherakey}&symbols={tickercode}"
 print(f"https://api.marketstack.com/v1/eod?access_key={otherakey}&symbols={tickercode}")
