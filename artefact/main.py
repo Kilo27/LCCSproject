da4e4248762b8383afb0df70e6af1ce4da32439b
Kilo27 <kyle.oseoighe@gmail.com>
2025-01-21 16:27:02 +0000

    line graph is broken, don't know why

--- artefact/main.py
+++ artefact/main.py
@@ -1,23 +1,42 @@
-from urllib.request import urlopen
-import json
-import time
-import requests
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
 
-akey="HB0CyUa7I01Oaj8FsR3sy4kk2NovfzKf"
-otherakey="a3d65b6e16f2672529ff996d85d98188"
-def get_jsonparsed_data(url):
-        with urlopen(url) as url:
-            data = json.load(url)
-            return data
-tickercode="GOOG"
+date=[]
+stockclose=[]
+Volume=[]
+stockopen=[]
+high=[]
+low=[]
+with open ("artefact\\aapl.csv", 'r') as file:
+    data = file.read()
+    l1=data.split("\n")
+    l1.pop()
+    for i in range(len(l1)):
+        l1[i]=l1[i].split(",")
+    for i in range(1, len(l1)):
+        date.append(l1[i][0])
+    for i in range(1, len(l1)):
+        stockclose.append(float(l1[i][1].replace("$", "")))
+    for i in range(1, len(l1)):
+        Volume.append(int(l1[i][2]))
+    for i in range(1, len(l1)):
+        stockopen.append(float(l1[i][3].replace("$", "")))
+    for i in range(1, len(l1)):
+        high.append(float(l1[i][1].replace("$", "")))
+    for i in range(1, len(l1)):
+        low.append(float(l1[i][1].replace("$", "")))
+    print(f"Date: {date}, Stock Close: {stockclose}, Volume: {Volume}, Stock Open: {stockopen}, High: {high}, Low: {low}")
 
-pastinfo=f"https://api.marketstack.com/v1/eod?access_key={otherakey}&symbols={tickercode}"
-print(f"https://api.marketstack.com/v1/eod?access_key={otherakey}&symbols={tickercode}")
-
-#print(get_jsonparsed_data(pastinfo))
-print(time.asctime())
-
-
-link = "https://api.marketstack.com/v1/eod?access_key={otherakey}&symbols={tickercode}"
-f = requests.get(link)
-print(f)
+#closing data graph
+x=np.array(date)
+y1=np.array(stockclose)
+y2=np.array(stockopen)
+y3=np.array(high)
+y4=np.array(low)
+plt.xlabel("Date")
+plt.plot(x,y1, label="Stock Close", linestyle="-")
+plt.plot(x,y2, label="Stock Open", linestyle="--", color="red")
+plt.plot(x,y3, label="High", linestyle="-.")
+plt.plot(x,y4, label="Low", linestyle=":")
+plt.show()
